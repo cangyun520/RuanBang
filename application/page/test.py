9161e0c7feb96dadd1d66a40536434c8ff264812
405367236@qq.com <billjc@521>
2018-06-08 17:30:16 +0800

    初始构建框架

---
+++ application/page/test.py
@@ -0,0 +1,35 @@
+# encoding:utf-8
+"""
+Created by Arvin.liu 15807146017
+Date :2018-06-08.
+"""
+import unittest
+
+
+class Count:
+    def __index__(self, a, b):
+        self.a = int(a)
+        self.b = int(b)
+
+    def add(self):
+        return self.a + self.b
+
+
+class TestCount(unittest.TestCase):
+    def setUp(self):
+        print('test start')
+
+    def test_add(self):
+        try:
+            j = Count(2, 3)
+            self.assertEqual(j.add(), 5)
+        except AssertionError as msg:
+            print(msg)
+        else:
+            print('Test pass')
+
+    def tearDown(self):
+        print('test end')
+
+if __name__ == '__main__':
+    unittest.main()
