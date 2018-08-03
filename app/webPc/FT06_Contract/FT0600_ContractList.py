c16e19f4d9a8b295e05d37b0b30c7fdbe39748c0
cangyun520 <405367236@qq.com>
2018-08-03 18:41:20 +0800

    excel导出后excel行数量校验

--- app/webPc/FT06_Contract/FT0600_ContractList.py
+++ app/webPc/FT06_Contract/FT0600_ContractList.py
@@ -1,8 +1,6 @@
 # encoding:utf-8
 from public.webClass import *
 from public.log import logger
-from public.downFile import *
-import xlrd
 from public.excelRead import *
 
 
@@ -15,49 +13,49 @@ prefs = {
 op.add_experimental_option('prefs', prefs)
 
 # 菜单目录
+_module = "合同管理"
 _menu = "签约明细"
 
 
-class ContractList(unittest.TestCase):
+class DataExport(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome(chrome_options=op)
         # 登录
         WebLogin.submit(self)
         # 打开菜单
-        WebMenu.full_text(self, "合同管理", _menu)
+        WebMenu.full_text(self, _module, _menu)
 
     """合同-导出"""
     def test_0602_02_export(self):
         """合同-导出"""
         dr = self.driver
         _time = time.strftime("%Y-%m-%d")
-        _tds = dr.find_elements_by_css_selector(".rb-gridview-sum-roll>table>tbody>tr>td")
-        for i in _tds:
-            if i.get_attribute("data-id") == "money":
-                # 文本转译
-                money = i.text
-                break
+        # 获取行总条数
+        sunCount = dr.find_element_by_class_name("rb-gridview-sumCount").text
 
+        # 导出到指定data/down目录并校验文件是否导出
         SAASPc.buttons(self, "导出")
         timesl(2)
-        try:
-            file_find("xlsx")
-        except FileExistsError as e:
-            dr.get_screenshot_as_file(PICTURE_PATH + "/webPc/test_0602_02_export.png")
-            unittest.expectedFailure("test_0602_02_export")
-
+        file_find("xlsx")
         f = _menu + "(" + _time + ")" + ".xlsx"
-        print(f)
-        p = file_path(f)
-        data = open_excel(p)
-        table = data.sheets()[0]
         try:
-            nrows = table.nrows
-            print(nrows-2)
-        except Exception as e:
+            f in file_find("xlsx")
+            print(f)
+        except FileExistsError as e:
+            logger.info(e)
+
+        # 读取excel内部数据，行总数
+        fpath = file_path(f)
+        data = open_excel(fpath)
+        table = data.sheets()[0]
+        nrows = table.nrows - 2
+
+        if int(sunCount) == nrows:
+            logger.info("行总计条数{0}".format(nrows))
+        else:
             dr.get_screenshot_as_file(PICTURE_PATH + "/webPc/test_0602_01_TotalMoneyCheck.png")
             unittest.expectedFailure("test_0602_01_TotalMoneyCheck")
-        timesl(1)
+            file_remove(f)
         file_remove(f)
 
     def tearDown(self):
